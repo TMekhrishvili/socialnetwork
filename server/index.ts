e8ba5dffee68a6232552e0da278f1d3479ecd2f1
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-04-14 20:02:33 +0400

    set up with typescript

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import next from 'next';
+// import getRoutes from './routes/index';
 
 
 const dev = process.env.NODE_ENV !== 'development';
@@ -11,7 +12,7 @@ app
     .prepare()
     .then(() => {
         const server = express();
-
+        // server.use('/api', getRoutes());
         server.get('*', (req: Request, res: Response) => {
             return handle(req, res);
         })

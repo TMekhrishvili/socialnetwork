bb2419600464764177ce03d13d9efbc1ffb9969e
Tamar Mekhrishvili <tmekh11@freeuni.edu.ge>
2022-04-15 21:06:41 +0400

    gave up with ssr

--- server/config/index.js
+++
@@ -1,7 +0,0 @@
-const port = process.env.port || 3000;
-const mongoURI = process.env.mongoURI || 'mongodb+srv://admin:admin123@cluster0.w0dvv.mongodb.net/tlsn?retryWrites=true&w=majority'
-
-module.exports = {
-    port,
-    mongoURI
-}
